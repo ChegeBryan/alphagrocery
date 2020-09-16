d79623ed43266aa15c422177e5626c80a39abea7
Chege Bryan <cbbryan8@gmail.com>
2020-09-16 14:01:31 +0300

    Remove category from Product fillable items

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['category_id', 'subcategory_id', 'store_id', 'product_parameter_id', 'product_name', 'product_description', 'product_price', 'product_image', 'product_quantity'];
+    protected $fillable = ['subcategory_id', 'store_id', 'product_parameter_id', 'product_name', 'product_description', 'product_price', 'product_image', 'product_quantity'];
 
     public function subcategory()
     {
