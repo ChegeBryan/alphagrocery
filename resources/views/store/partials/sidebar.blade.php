562a1fe53ff8562a54ccfece8002b0faaede561f
Chege Bryan <cbbryan8@gmail.com>
2020-09-16 12:39:17 +0300

    Update links on store sidebar

--- resources/views/store/partials/sidebar.blade.php
+++ resources/views/store/partials/sidebar.blade.php
@@ -24,7 +24,7 @@
 
       <!-- Heading -->
       <div class="sidebar-heading">
-        Interface
+        Products
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
@@ -32,13 +32,13 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
            aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+          <span>My Products</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Actions:</h6>
+            <a class="collapse-item" href="{{ route('products.index') }}">Add Products</a>
+            <a class="collapse-item" href="{{ route('products.index') }}">Manage Products</a>
           </div>
         </div>
       </li>
