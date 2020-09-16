6f2397ee3c7a989c69ee0c3c6fbea8029831f0c1
Chege Bryan <cbbryan8@gmail.com>
2020-09-16 14:00:22 +0300

    Remove category from productsfactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,7 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'category_id' => factory(App\Category::class),
         'subcategory_id' => factory(App\Subcategory::class),
         'store_id' => factory(App\Store::class),
         'product_parameter_id' => factory(App\ProductParameter::class),
