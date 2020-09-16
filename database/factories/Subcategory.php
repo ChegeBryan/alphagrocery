a79e9916171d112c8d29dad688760926f75d42e5
Chege Bryan <cbbryan8@gmail.com>
2020-09-16 11:16:44 +0300

    Modify subcategory factory

--- database/factories/Subcategory.php
+++ database/factories/Subcategory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Subcategory::class, function (Faker $faker) {
     return [
-        'subcategory_name' => $faker->name,
+        'subcategory_name' => $faker->word,
         'subcategory_image' => '1600094558.png',
         'category_id' => factory(App\Category::class),
     ];
