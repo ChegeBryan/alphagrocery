780143b70646c06197e48e8da3ae228cd2feb816
Chege Bryan <cbbryan8@gmail.com>
2020-09-17 11:26:16 +0300

    Update templates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,12 +54,22 @@
       <img src="{{ asset('storage/products/'.$product->product_image) }}" class="card-img-top img-fluid" width="100px"
            height="100px" alt="...">
       <div class="card-body">
-        <div class="d-flex justify-content-between">
-          <span class="font-weight-bold text-info">Ksh. {{$product->product_price}}</span>
-          <a href="#" class="text-dark"><i class="fa fa-cart-plus fa-fw fa-2x"></i></a>
-        </div>
+        <h6 class="card-title font-weight-bold">{{ucfirst($product->product_name)}}</h6>
+        <p class="card-subtitle font-weight-bold">Ksh. {{$product->product_price}}</p>
         <span class="text-muted">per. {{$product->parameter->parameter}}</span>
-        <p class="card-text">{{ucfirst($product->product_name)}}</p>
+        <p class="card-text">{{$product->product_description}}</p>
+        <p class="card-text text-muted"><i class="fas fa-store"></i>&nbsp;{{$product->store->name}}</p>
+        <form action="{{ route('cart.store') }}" method="POST">
+          {{ csrf_field() }}
+          <input type="hidden" value="{{ $product->id }}" id="id" name="id">
+          <input type="hidden" value="{{ $product->product_name }}" id="name" name="name">
+          <input type="hidden" value="{{ $product->product_price }}" id="price" name="price">
+          <input type="hidden" value="{{ $product->product_image }}" id="img" name="img">
+          <input type="hidden" value="1" id="quantity" name="quantity">
+
+          <button class="btn btn-secondary btn-sm btn-block" class="tooltip-test" title="add to cart">
+            <i class="fa fa-cart-plus fa-fw"></i> Add to Cart
+        </form>
       </div>
     </div>
     @endforeach
