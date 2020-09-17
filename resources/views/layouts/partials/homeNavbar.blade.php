3d93e33209b5e448e027283db80a4625d9f7972e
Chege Bryan <cbbryan8@gmail.com>
2020-09-17 12:01:11 +0300

    Add cart drop down

--- resources/views/layouts/partials/homeNavbar.blade.php
+++ resources/views/layouts/partials/homeNavbar.blade.php
@@ -21,11 +21,9 @@
            <a class="nav-link" href="{{ route('home') }}">{{ __('Checkout') }}</a>
          </li>
        </ul>
-
        <!-- Right Side Of Navbar -->
        <ul class="navbar-nav ml-auto">
-         <li class="nav-item">
-           <!-- <a class="nav-link" href="{{ route('home') }}">{{ __('Cart') }}</a> -->
+         <li class="nav-item dropdown">
            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
               aria-haspopup="true" aria-expanded="false">
              <span class="badge badge-pill badge-dark">
@@ -36,9 +34,10 @@
            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown"
                 style="width: 450px; padding: 0px; border-color: #9DA0A2">
              <ul class="list-group" style="margin: 20px;">
-
+               @include('layouts.partials.cart-drop')
              </ul>
 
+           </div>
          </li>
 
          @guest
