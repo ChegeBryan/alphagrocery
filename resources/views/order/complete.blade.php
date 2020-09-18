9f2a28fe43e0541ebeaccf3562d8db7787af2a6c
Chege Bryan <cbbryan8@gmail.com>
2020-09-18 13:15:28 +0300

    Add order completion template

---
+++ resources/views/order/complete.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title') Mama Mboga @endsection
+
+@section('content')
+<!-- Picks of the day -->
+<div class="d-flex justify-content-center align-items-center py-5">
+  <i class="fas fa-check-circle text-success fa-10x"></i>
+</div>
+<div class="d-flex justify-content-center align-items-center py-5">
+  <h3>Order Placed.</h3>
+</div>
+<div class="d-flex justify-content-center align-items-center py-5">
+  <h4>Thank you. Your order is on your way.</h4>
+</div>
+@endsection
