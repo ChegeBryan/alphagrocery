d5efc6f3d220e4cbbf9c352c32f87ab412576785
Chege Bryan <cbbryan8@gmail.com>
2020-09-18 12:41:56 +0300

    Add method to save order to database

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
+use Session;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function saveOrder()
+    {
+        $items = \Cart::getContent();
+        foreach ($items as $item) {
+            $item = new Order([
+                'customer_id' => Auth::user()->id,
+                'shipping_id' => Session::get('deliveryinfo'),
+                'product_id' => $item->id,
+                'store_id' => $item->attributes->store,
+                'store_name' => $item->attributes->store_name,
+                'product_name' => $item->name,
+                'product_image' => $item->attributes->image,
+                'product_quantity' => $item->quantity,
+                'order_subtotal' => $item->getPriceSum(),
+            ]);
+            $item->save();
+        }
+    }
 }
