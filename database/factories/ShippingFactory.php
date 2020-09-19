e57abddfb976ddd60416142e87c11a2da8ae6831
Chege Bryan <cbbryan8@gmail.com>
2020-09-19 10:34:20 +0300

    Update shippingfactory model class

--- database/factories/ShippingFactory.php
+++ database/factories/ShippingFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Shipping;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Shipping::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'phone_number' => $faker->e164PhoneNumber,
