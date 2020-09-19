7993a40e113fc6e01eebc6313ed1d3b9a5b756c0
Chege Bryan <cbbryan8@gmail.com>
2020-09-19 14:54:20 +0300

    Clean up mark up

--- resources/views/admin/subcategory/edit.blade.php
+++ resources/views/admin/subcategory/edit.blade.php
@@ -7,201 +7,89 @@
 @section('body')
 <div class="container-fluid">
 
-  <!-- Page Heading -->
-  <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Sub Category</h1>
-    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-         class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-  </div>
-
-  <!-- Content Row -->
-  <div class="row">
-
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Category</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-calendar fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Sub Category</h1>
     </div>
+    <!-- Content Row -->
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-success shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div class="row">
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-info shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+        <!-- Area Chart -->
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <h6 class="card-header m-0 font-weight-bold text-primary py-3">Update Subcategory</h6>
+                <!-- Card Body -->
+                <div class="card-body">
+                    @if(session()->get('success'))
+                    <div class="alert alert-success">
+                        {{ session()->get('success') }}
+                    </div>
+                    @endif
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <br />
+                    @endif
+                    <form method="POST" action="{{ route('subcategory.update', $subcategory->id) }}" enctype="multipart/form-data">
+                        @method('PATCH')
+                        @csrf
+
+                        <div class=" form-group">
+                            <label for="subcategory_name">{{ __('Sub-Category Name') }}</label>
+                            <input type="text" class="form-control" name="subcategory_name" id="subcategory_name" value="{{ $subcategory->subcategory_name }}">
+                        </div>
+
+                        <div class="form-text mb-3">Current Category: {{$subcategory->category->category_name}}</div>
+
+                        <select class="custom-select" name="category">
+                            <option selected disabled>Select New category</option>
+                            @foreach($categories as $category)
+                            <option value="{{ $category->id }}">{{$category->category_name}}</option>
+                            @endforeach
+                        </select>
+
+                        <div class="form-row mt-3">
+                            <div class="form-group col-md-6">
+                                <img class="img-fluid w-25" src="{{asset('images/category/'.$subcategory->subcategory_image)}}">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="subcategory_image" name="subcategory_image">
+                                    <label class="custom-file-label" for="subcategory_image">Update subcategory pic</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block mt-3">
+                            {{ __('Update Subcategory') }}
+                        </button>
+                    </form>
                 </div>
-                <div class="col">
-                  <div class="progress progress-sm mr-2">
-                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50"
-                         aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
+            </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="col-xl-4 col-lg-5">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+
                 </div>
-              </div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                <!-- Card Body -->
+                <div class="card-body">
 
-    <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Pending Requests</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-comments fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Content Row -->
-
-  <div class="row">
-
-    <!-- Area Chart -->
-    <div class="col-xl-8 col-lg-7">
-      <div class="card shadow mb-4">
-        <!-- Card Header - Dropdown -->
-        <h6 class="card-header m-0 font-weight-bold text-primary py-3">Update Subcategory</h6>
-        <!-- Card Body -->
-        <div class="card-body">
-          @if(session()->get('success'))
-          <div class="alert alert-success">
-            {{ session()->get('success') }}
-          </div>
-          @endif
-          @if ($errors->any())
-          <div class="alert alert-danger">
-            <ul>
-              @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-              @endforeach
-            </ul>
-          </div>
-          <br />
-          @endif
-          <form method="POST" action="{{ route('subcategory.update', $subcategory->id) }}"
-                enctype="multipart/form-data">
-            @method('PATCH')
-            @csrf
-
-            <div class=" form-group">
-              <label for="subcategory_name">{{ __('Sub-Category Name') }}</label>
-              <input type="text" class="form-control" name="subcategory_name" id="subcategory_name"
-                     value="{{ $subcategory->subcategory_name }}">
-            </div>
-
-            <div class="form-text mb-3">Current Category: {{$subcategory->category->category_name}}</div>
-
-            <select class="custom-select" name="category">
-              <option selected disabled>Select New category</option>
-              @foreach($categories as $category)
-              <option value="{{ $category->id }}">{{$category->category_name}}</option>
-              @endforeach
-            </select>
-
-            <div class="form-row mt-3">
-              <div class="form-group col-md-6">
-                <img class="img-fluid w-25" src="{{asset('images/category/'.$subcategory->subcategory_image)}}">
-              </div>
-              <div class="form-group col-md-6">
-                <div class="custom-file">
-                  <input type="file" class="custom-file-input" id="subcategory_image" name="subcategory_image">
-                  <label class="custom-file-label" for="subcategory_image">Update subcategory pic</label>
                 </div>
-              </div>
             </div>
-
-            <button type="submit" class="btn btn-primary btn-block mt-3">
-              {{ __('Update Subcategory') }}
-            </button>
-          </form>
         </div>
-      </div>
     </div>
-
-    <!-- Pie Chart -->
-    <div class="col-xl-4 col-lg-5">
-      <div class="card shadow mb-4">
-        <!-- Card Header - Dropdown -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-          <div class="dropdown no-arrow">
-            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-               aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-              <div class="dropdown-header">Dropdown Header:</div>
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
-        <!-- Card Body -->
-        <div class="card-body">
-          <div class="chart-pie pt-4 pb-2">
-            <canvas id="myPieChart"></canvas>
-          </div>
-          <div class="mt-4 text-center small">
-            <span class="mr-2">
-              <i class="fas fa-circle text-primary"></i> Direct
-            </span>
-            <span class="mr-2">
-              <i class="fas fa-circle text-success"></i> Social
-            </span>
-            <span class="mr-2">
-              <i class="fas fa-circle text-info"></i> Referral
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
 </div>
 @endsection
