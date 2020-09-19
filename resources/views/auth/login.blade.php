1ffaab91528fb6587c6ead406c9bf87a2f31ce3b
Chege Bryan <cbbryan8@gmail.com>
2020-09-19 13:03:04 +0300

    Add additional user login links

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,59 +2,62 @@
 
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-5">
-      <div class="card">
-        <div class="card-body">
-          <h4 class="card-title text-center">{{ isset($url) ? ucwords($url) : ""}} {{ __('Login') }}</h4>
-          @isset($url)
-          <form method="POST" action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
-            @else
-            <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-              @endisset
-              @csrf
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title text-center">{{ isset($url) ? ucwords($url) : ""}} {{ __('Login') }}</h4>
+                    @isset($url)
+                    <form method="POST" action='{{ url("login/$url") }}' aria-label="{{ __('Login') }}">
+                        @else
+                        <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                            @endisset
+                            @csrf
 
-              <div class="form-group">
-                <label for="email">{{ __('E-Mail Address') }}</label>
+                            <div class="form-group">
+                                <label for="email">{{ __('E-Mail Address') }}</label>
 
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                       value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-                </span>
-                @enderror
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
 
-              </div>
+                            </div>
 
-              <div class="form-group">
-                <label for="password">{{ __('Password') }}</label>
+                            <div class="form-group">
+                                <label for="password">{{ __('Password') }}</label>
 
 
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                       name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                @error('password')
-                <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-                </span>
-                @enderror
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
 
-              </div>
-              <button type="submit" class="btn btn-primary btn-block">
-                {{ __('Login') }}
-              </button>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">
+                                {{ __('Login') }}
+                            </button>
 
-              @if (Route::has('password.request'))
-              <a class="btn btn-link" href="{{ route('password.request') }}">
-                {{ __('Forgot Your Password?') }}
-              </a>
-              @endif
-            </form>
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                            @endif
+                        </form>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mt-3">
+                <a class="card-link" href="{{ route('login') }}">Customer</a>
+                <a class="card-link" href="{{ url('login/store') }}">Store</a>
+                <a class="card-link" href="{{ url('login/admin') }}">Admin</a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 @endsection
