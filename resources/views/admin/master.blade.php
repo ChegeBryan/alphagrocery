f662f9f51bdf247c081ebd84a29f0ce7e4468fb6
Chege Bryan <cbbryan8@gmail.com>
2020-09-19 19:10:12 +0300

    Add favicon

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
+  <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥕</text></svg>">
     <title>@yield('title') - Dashboard</title>
 
     <!-- Custom fonts for this template-->
 
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
 
     <script src="{{ asset('js/app.js') }}"></script>
 
@@ -20,58 +22,58 @@
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     @yield('custom-css')
 
-</head>
+  </head>
 
-<body id="page-top">
+  <body id="page-top">
 
     <!-- Page Wrapper -->
     <div id="wrapper">
 
-        <!-- Sidebar -->
-        @include('admin.partials.sidebar');
-        <!-- End of Sidebar -->
+      <!-- Sidebar -->
+      @include('admin.partials.sidebar');
+      <!-- End of Sidebar -->
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+        <!-- Main Content -->
+        <div id="content">
 
-                <!-- Topbar -->
-                @include('admin.partials.topbar')
-                <!-- End of Topbar -->
+          <!-- Topbar -->
+          @include('admin.partials.topbar')
+          <!-- End of Topbar -->
 
-                <!-- Begin Page Content -->
-                @yield('body')
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Alpha Grocery 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+          <!-- Begin Page Content -->
+          @yield('body')
+          <!-- /.container-fluid -->
 
         </div>
-        <!-- End of Content Wrapper -->
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Alpha Grocery 2020</span>
+            </div>
+          </div>
+        </footer>
+        <!-- End of Footer -->
+
+      </div>
+      <!-- End of Content Wrapper -->
 
     </div>
     <!-- End of Page Wrapper -->
 
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
+      <i class="fas fa-angle-up"></i>
     </a>
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
     @yield('custom-script')
-</body>
+  </body>
 
 </html>
