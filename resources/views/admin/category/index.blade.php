962e67f40d531359910ab444d8dc558afb6fb418
Chege Bryan <cbbryan8@gmail.com>
2020-09-19 13:59:38 +0300

    Update admin pages

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -10,54 +10,54 @@
 @section('body')
 <div class="container-fluid">
 
-  <!-- Page Heading -->
-  <h1 class="h3 mb-2 text-gray-800">Categories</h1>
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Categories</h1>
 
-  <!-- DataTales Example -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Product Categories</h6>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Product Categories</h6>
+        </div>
+        <div class="card-body">
+            @if(session()->get('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+            @endif
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dt" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Actions</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($categories as $category)
+                        <tr>
+                            <td><img src="{{ asset('storage/categories/'.$category->category_image )}}" alt="" class="rounded" width="75px" height="75px"></td>
+                            <td>{{$category->category_name}}</td>
+                            <td>{{$category->category_description}}</td>
+                            <td>
+                                <a href="{{ route('category.edit', $category->id)}}" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td>
+                                <form action="{{ route('category.destroy', $category->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-      @if(session()->get('success'))
-      <div class="alert alert-success">
-        {{ session()->get('success') }}
-      </div>
-      @endif
-      <div class="table-responsive">
-        <table class="table table-bordered" id="dt" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Description</th>
-              <th colspan=2>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($categories as $category)
-            <tr>
-              <td><img src="{{ asset('storage/categories/'.$category->category_image )}}" alt="" class="rounded"
-                     width="75px" height="75px"></td>
-              <td>{{$category->category_name}}</td>
-              <td>{{$category->category_description}}</td>
-              <td>
-                <a href="{{ route('category.edit', $category->id)}}" class="btn btn-primary">Edit</a>
-              </td>
-              <td>
-                <form action="{{ route('category.destroy', $category->id)}}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
 </div>
 @endsection
 @section('custom-script')
