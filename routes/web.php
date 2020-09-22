b07b3a4b891fddde49917973512810ec77a4e2c3
Chege Bryan <cbbryan8@gmail.com>
2020-09-22 08:55:06 +0300

    Update routes used

--- routes/web.php
+++ routes/web.php
@@ -31,24 +31,24 @@ Route::post('/register/store', 'Auth\RegisterController@createStore')->name('reg
 //Route::view('/home', 'home')->middleware('auth');
 Route::group(['middleware' => 'auth:admin', 'prefix' => 'admin'], function () {
     Route::redirect('', '/admin/category', 301)->name('admin.home');
-    Route::resource('category', 'CategoryController');
-    Route::resource('subcategory', 'SubcategoryController');
-    Route::resource('prodparameter', 'ProductParameterController');
+    Route::resource('category', 'CategoryController')->except(['show']);
+    Route::resource('subcategory', 'SubcategoryController')->except(['show']);
+    Route::resource('prodparameter', 'ProductParameterController')->except(['show']);
 });
 
 Route::group(['middleware' => 'auth:store', 'prefix' => 'store'], function () {
     Route::redirect('', '/store/products', 301)->name('store.home');
-    Route::resource('products', 'ProductController');
+    Route::resource('products', 'ProductController')->except(['show']);
     Route::get('orders/customer/{id}', 'OrderManagementController@customerOrders')->name('orders.customer');
-    Route::resource('orders', 'OrderManagementController');
+    Route::resource('orders', 'OrderManagementController')->except(['create', 'store', 'edit']);
 });
 
 
 Route::post('/clear', 'CartController@clear')->name('cart.clear');
-Route::resource('cart', 'CartController');
+Route::resource('cart', 'CartController')->except(['show']);
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('checkout', 'CheckoutController');
+    Route::resource('checkout', 'CheckoutController')->except(['show', 'edit', 'update', 'destroy']);
     Route::post('/order/place', 'OrderController@saveOrder')->name('order.place');
     Route::get('/order/complete', 'OrderController@orderComplete')->name('order.complete');
 });
