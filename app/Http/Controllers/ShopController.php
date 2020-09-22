b928a10b7b68fb24b4a3cac5144f3ce3d46f0d37
Chege Bryan <cbbryan8@gmail.com>
2020-09-22 09:27:37 +0300

    Add shop controller

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function shopByCategory($name)
+    {
+    }
+
+    public function shopBySubcategory($name)
+    {
+    }
+
+    public function shopByStore($name)
+    {
+    }
+}
